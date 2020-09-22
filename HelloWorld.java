0b3b4ba692af9cc6dc0d88d289bd1dcc5a8c3c86
devanagemafalesta <g.dyavana@gmail.com>
2020-09-22 23:07:45 +0700

    Latihan Push

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+	
+	public static void main (String[] args){
+		System.out.println("Hello Devana");
+		System.out.println("Nama : Devana Gema Falesta");
+		System.out.println("Kelas : D3IF4401");
+		System.out.println("Nim : 6706201102");
+	}
+}
